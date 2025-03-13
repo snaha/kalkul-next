d97cd6e57148a228b59f4362e47ef2b90fef1733
Tomáš Hlávka <137988631+hlavkat@users.noreply.github.com>
2025-03-13 16:33:12 +0100

    fix: cascade duplicate rollback if fails (#495)

--- src/lib/cascade.ts
+++ src/lib/cascade.ts
@@ -5,45 +5,59 @@ import { transactionStore } from './stores/transaction.svelte'
 import type { InvestmentWithColorIndex, Transaction } from './types'
 
 export async function cascadeDuplicatePortfolio(clientId: number, portfolioId: number) {
-	const portfolios = portfolioStore.filter(clientId)
-	const originalPortfolio = portfolios.find((portfolio) => portfolio.id === portfolioId)
-	if (!originalPortfolio) {
-		return
+	let duplicatedPortfolioId: number | undefined = undefined
+	try {
+		const portfolios = portfolioStore.filter(clientId)
+		const originalPortfolio = portfolios.find((portfolio) => portfolio.id === portfolioId)
+		if (!originalPortfolio) {
+			return
+		}
+
+		const duplicatedPortfolio = {
+			...originalPortfolio,
+			id: undefined,
+			link: null,
+			name: originalPortfolio.name + ' - Copy',
+		}
+
+		duplicatedPortfolioId = await adapters.addPortfolio(duplicatedPortfolio)
+
+		const investments = investmentStore.filter(portfolioId)
+		for (const investment of investments) {
+			await cascadeDuplicateInvestment(investment, duplicatedPortfolioId)
+		}
+
+		return duplicatedPortfolioId
+	} catch (e) {
+		if (duplicatedPortfolioId) await adapters.deletePortfolio({ id: duplicatedPortfolioId })
+		console.error(e)
+		throw e
 	}
-
-	const duplicatedPortfolio = {
-		...originalPortfolio,
-		id: undefined,
-		link: null,
-		name: originalPortfolio.name + ' - Copy',
-	}
-
-	const duplicatedPortfolioId = await adapters.addPortfolio(duplicatedPortfolio)
-
-	const investments = investmentStore.filter(portfolioId)
-	for (const investment of investments) {
-		await cascadeDuplicateInvestment(investment, duplicatedPortfolioId)
-	}
-
-	return duplicatedPortfolioId
 }
 
 export async function cascadeDuplicateInvestment(
 	investment: InvestmentWithColorIndex,
 	duplicatedPortfolioId: number,
 ) {
-	const duplicatedInvestment = {
-		...investment,
-		id: undefined,
-		portfolio: duplicatedPortfolioId,
-	}
-	delete duplicatedInvestment.colorIndex
+	let duplicatedInvestmentId: number | undefined = undefined
+	try {
+		const duplicatedInvestment = {
+			...investment,
+			id: undefined,
+			portfolio: duplicatedPortfolioId,
+		}
+		delete duplicatedInvestment.colorIndex
 
-	const duplicatedInvestmentId = await adapters.addInvestment(duplicatedInvestment)
+		duplicatedInvestmentId = await adapters.addInvestment(duplicatedInvestment)
 
-	const transactions = transactionStore.filter(investment.id)
-	for (const transaction of transactions) {
-		await cascadeDuplicateTransaction(transaction, duplicatedInvestmentId)
+		const transactions = transactionStore.filter(investment.id)
+		for (const transaction of transactions) {
+			await cascadeDuplicateTransaction(transaction, duplicatedInvestmentId)
+		}
+	} catch (e) {
+		if (duplicatedInvestmentId) await adapters.deleteInvestment({ id: duplicatedInvestmentId })
+		console.error(e)
+		throw e
 	}
 }
 
@@ -51,11 +65,15 @@ async function cascadeDuplicateTransaction(
 	transaction: Transaction,
 	duplicatedInvestmentId: number,
 ) {
-	const duplicatedTransaction = {
-		...transaction,
-		id: undefined,
-		investment_id: duplicatedInvestmentId,
+	try {
+		const duplicatedTransaction = {
+			...transaction,
+			id: undefined,
+			investment_id: duplicatedInvestmentId,
+		}
+		await adapters.addTransaction(duplicatedTransaction)
+	} catch (e) {
+		console.error(e)
+		throw e
 	}
-
-	await adapters.addTransaction(duplicatedTransaction)
 }
