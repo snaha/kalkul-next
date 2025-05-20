a729c2492ed98f474606f3ab15a6b979a0395808
Vojtech Simetka <vojtech@simetka.cz>
2025-05-20 09:36:42 +0200

    feat: add terms of service and privacy policy (#649)

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,336 @@
+<script lang="ts">
+	import CustomHeader from '$lib/components/custom-header.svelte'
+	import Typography from '$lib/components/ui/typography.svelte'
+</script>
+
+<CustomHeader />
+
+<main>
+	<div class="content">
+		<Typography variant="h2"
+			>Zásady ochrany osobních údajů uživatelů aplikace „kalkul.app“</Typography
+		>
+
+		<ol>
+			<li class="h4">Úvodní ustanovení</li>
+			<ol>
+				<li>
+					Pro účely zpracování osobních údajů je správcem společnost kalkul.app s.r.o., IČO:
+					22570110, sídlem: Stará silnice 2314/3, Předměstí, 746 01 Opava, sp. zn. C 98681 vedená u
+					Krajského soudu v Ostravě (dále jen „Provozovatel“), která určuje účely a prostředky
+					tohoto zpracování.
+				</li>
+				<li>
+					Osobní údaje Uživatele jsou zpracovávány pouze v rozsahu nezbytném pro poskytování Služby,
+					zajištění její funkčnosti a provoz webového rozhraní, včetně nezbytné komunikace mezi
+					Uživatelem a Provozovatelem.
+				</li>
+				<li>
+					Zpracování osobních údajů probíhá v souladu s Nařízením Evropského parlamentu a Rady (EU)
+					2016/679, o ochraně fyzických osob v souvislosti se zpracováním osobních údajů a o volném
+					pohybu těchto údajů (dále jen „Nařízení“).
+				</li>
+				<li>
+					Provozovatel jmenoval pověřence pro ochranu osobních údajů („DPO“), který dohlíží na
+					soulad zpracování osobních údajů s Nařízením. Pověřence je možné kontaktovat
+					prostřednictvím elektronické pošty na adrese: support@kalkul.app .
+				</li>
+				<li>
+					Pojem Uživatel a případné další pojmy uvozené velkými v písmeny jsou používány se stejným
+					významem jako v obchodních podmínkách provozování aplikace „kalkul.app“, jejichž přílohou
+					jsou tyto zásady.
+				</li>
+			</ol>
+			<li class="h4">Kategorie osobních údajů</li>
+			<ol>
+				<li>
+					Provozovatel zpracovává osobní údaje pouze v rozsahu nezbytném pro zajištění přístupu
+					Uživatele ke Službě a její bezpečné poskytování.
+				</li>
+				<li>
+					Za tímto účelem Provozovatel uchovává identifikační a přihlašovací údaje Uživatele,
+					konkrétně e-mailovou adresu a heslo zvolené Uživatelem při registraci.
+				</li>
+				<li>
+					Platební údaje Uživatele, včetně fakturačních údajů a informací o předplatném, jsou
+					zpracovávány výhradně prostřednictvím služby Stripe, která v tomto rozsahu vystupuje jako
+					samostatný správce osobních údajů.
+				</li>
+				<li>
+					Provozovatel nemá přístup k údajům o platebních kartách ani k jiným údajům, které Stripe
+					zpracovává za účelem zajištění platebního styku.
+				</li>
+			</ol>
+			<li class="h4">Účel zpracování osobních údajů</li>
+			<ol>
+				<li>
+					Osobní údaje Uživatele jsou zpracovávány výhradně pro účely uvedené v těchto zásadách
+					ochrany osobních údajů.
+				</li>
+				<li>
+					Osobní údaje Uživatele jsou zpracovávány zejména za účelem:
+					<ol>
+						<li>umožnění přístupu k Aplikaci a jejího užívání Uživatelem,</li>
+						<li>
+							zajištění provozu Uživatelského účtu, včetně ověření přihlašovacích údajů a komunikace
+							mezi Uživatelem a Provozovatelem,
+						</li>
+						<li>
+							plnění zákonných povinností Provozovatele vyplývajících zejména z právních předpisů v
+							oblasti ochrany osobních údajů, daňových a účetních předpisů,
+						</li>
+						<li>
+							ochrany práv a oprávněných zájmů Provozovatele, zejména při řešení technických,
+							bezpečnostních nebo uživatelských incidentů, vymáhání pohledávek nebo uplatňování
+							právních nároků,
+						</li>
+						<li>
+							řízení vztahu s Uživatelem, včetně vyřizování podnětů, dotazů a uplatňování práv
+							souvisejících se zpracováním osobních údajů.
+						</li>
+					</ol>
+				</li>
+				<li>
+					Za účelem přímého marketingu může Provozovatel využívat kontaktní údaje Uživatele, zejména
+					e-mailovou adresu, a to na základě svého oprávněného zájmu. Uživatel má vždy možnost
+					takové zpracování odmítnout, a to prostřednictvím každého marketingového sdělení nebo
+					kontaktováním Provozovatele.
+				</li>
+				<li>
+					Zpracování osobních údajů nad rámec výše uvedených účelů je možné pouze na základě
+					svobodného a výslovného souhlasu Uživatele. Udělení takového souhlasu není podmínkou
+					užívání Služby a jeho neudělení nemá vliv na přístup Uživatele k Aplikaci.
+				</li>
+			</ol>
+			<li class="h4">Podmínky zpracování a uchování osobních údajů</li>
+			<ol>
+				<li>
+					Osobní údaje Uživatele jsou uchovávány pouze po dobu nezbytnou k naplnění účelu jejich
+					zpracování, a to v souladu s platnými právními předpisy.
+				</li>
+				<li>
+					Údaje zpracovávané v souvislosti s užíváním Služby jsou uchovávány po dobu trvání
+					Uživatelského účtu, případně po dobu, po kterou je to nezbytné pro splnění právní
+					povinnosti nebo ochranu oprávněných zájmů Provozovatele. Po zániku Uživatelského účtu
+					mohou být nezbytné údaje uchovávány po dobu maximálně 10 let, zejména pro účely splnění
+					právních a účetních povinností.
+				</li>
+				<li>
+					Údaje zpracovávané na základě souhlasu Uživatele jsou uchovávány po dobu trvání tohoto
+					souhlasu. Samotný souhlas, jeho změna nebo odvolání mohou být uchovávány po dobu platnosti
+					souhlasu a dále po dobu 10 let od jeho odvolání, a to z důvodu ochrany oprávněných zájmů
+					Provozovatele.
+				</li>
+				<li>
+					Osobní údaje jsou zpracovávány výhradně v elektronické podobě, a to automatizovaným
+					způsobem v rámci zabezpečeného informačního systému Provozovatele.
+				</li>
+				<li>
+					Při zpracování osobních údajů nejsou využívány prostředky automatizovaného rozhodování,
+					které by měly právní účinky nebo obdobný významný dopad na Uživatele. Provozovatel dále
+					neprovádí profilování, jehož cílem by bylo hodnocení osobních aspektů Uživatele.
+				</li>
+			</ol>
+
+			<li class="h4">Příjemci osobních údajů</li>
+			<ol>
+				<li>Osobní údaje Uživatele jsou zásadně zpracovávány výlučně v rámci Provozovatele.</li>
+				<li>
+					Předání osobních údajů třetím osobám může nastat pouze v případech stanovených právními
+					předpisy, zejména za účelem splnění zákonné povinnosti nebo ochrany právních nároků
+					Provozovatele. O jednotlivých příjemcích osobních údajů je Uživatel informován v souladu s
+					požadavky platných právních předpisů.
+				</li>
+				<li>
+					Provozovatel dále využívá služeb třetích stran pro zajištění technického a provozního
+					fungování Aplikace. Mezi tyto subjekty patří zejména Provozovatelé hostingových a IT
+					služeb a Provozovatel platební brány Stripe, jehož prostřednictvím je zajištěna správa
+					předplatného.
+				</li>
+				<li>
+					Třetí strany zpracovávají osobní údaje výhradně na základě smlouvy o zpracování osobních
+					údajů uzavřené s Provozovatelem a v souladu s platnou právní úpravou.
+				</li>
+				<li>
+					Osobní údaje nejsou předávány do třetích zemí mimo Evropskou unii ani žádné mezinárodní
+					organizaci, pokud není v konkrétním případě výslovně stanoveno jinak.
+				</li>
+			</ol>
+			<li class="h4">Způsoby získávání osobních údajů</li>
+			<ol>
+				<li>
+					Osobní údaje Uživatele získává Provozovatel zejména:
+					<ol>
+						<li>
+							přímo od Uživatele při registraci a při využívání Služby prostřednictvím webového
+							rozhraní Aplikace,
+						</li>
+						<li>
+							v rámci technické správy Uživatelského účtu a komunikace související s poskytováním
+							Služby,
+						</li>
+						<li>
+							od Uživatele jakožto zájemce o využití Služby při vyplnění kontaktních formulářů nebo
+							jiných údajů v rámci webového rozhraní Aplikace.
+						</li>
+					</ol>
+				</li>
+				<li>
+					Provozovatel nezískává osobní údaje z veřejných rejstříků ani z jiných externích databází,
+					pokud to nevyžaduje zvláštní právní předpis nebo zvláštní situace, na kterou bude Uživatel
+					předem upozorněn.
+				</li>
+			</ol>
+			<li class="h4">Práva subjektů osobních údajů</li>
+			<ol>
+				<li>
+					Provozovatel při shromažďování, zpracování a uchovávání osobních údajů plně respektuje
+					práva Uživatele jakožto subjektu údajů, jak vyplývají z příslušných právních předpisů,
+					zejména Nařízení.
+				</li>
+				<li>
+					Uživatel má zejména následující práva, která může kdykoli uplatnit:
+					<ol>
+						<li>
+							právo na přístup k osobním údajům – právo získat informaci o tom, zda jsou osobní
+							údaje zpracovávány, a pokud ano, v jakém rozsahu a za jakým účelem; v případě
+							opakujících se, zjevně bezdůvodných nebo nepřiměřených žádostí může Provozovatel
+							účtovat přiměřený poplatek nebo žádosti nevyhovět,
+						</li>
+						<li>právo na opravu nebo doplnění nepřesných či neúplných údajů,</li>
+						<li>
+							právo na výmaz – zejména pokud údaje již nejsou potřebné pro daný účel, byl odvolán
+							souhlas nebo byla vznesena námitka proti zpracování a neexistují převažující oprávněné
+							důvody,
+						</li>
+						<li>
+							právo na omezení zpracování – například pokud Uživatel popírá přesnost údajů nebo
+							pokud jsou údaje potřebné pouze pro určení, výkon nebo obhajobu právních nároků,
+						</li>
+						<li>
+							právo na přenositelnost údajů – právo získat osobní údaje, které byly Provozovateli
+							poskytnuty, ve strukturovaném a strojově čitelném formátu a předat je jinému správci,
+						</li>
+						<li>
+							právo odvolat souhlas – pokud je zpracování založeno na souhlasu, může jej Uživatel
+							kdykoli zcela nebo zčásti odvolat; tím není dotčena zákonnost zpracování před
+							odvoláním souhlasu,
+						</li>
+						<li>
+							právo podat stížnost – Uživatel má právo obrátit se se stížností na dozorový orgán,
+							kterým je Úřad pro ochranu osobních údajů (www.uoou.cz).
+						</li>
+					</ol>
+				</li>
+				<li>
+					V souvislosti s výkonem svých práv, dotazy nebo požadavky ohledně zpracování osobních
+					údajů může Uživatel kontaktovat Provozovatele prostřednictvím e-mailové adresy
+				</li>
+			</ol>
+
+			<li class="h4">Postavení Provozovatele při zpracování údajů zadaných Uživatelem</li>
+			<ol>
+				<li>
+					V případech, kdy Uživatel vkládá prostřednictvím Aplikace osobní údaje třetích osob
+					(například svých klientů), činí tak v postavení správce osobních údajů. Provozovatel vůči
+					těmto údajům vystupuje jako zpracovatel ve smyslu Nařízení a provádí jejich zpracování
+					výhradně na základě pokynů Uživatele, a to za účelem poskytování funkčnosti Služby. Třetí
+					strany zajišťující technické prostředky pro provoz Aplikace jsou v tomto rozsahu
+					považovány za další zpracovatele.
+				</li>
+				<li>
+					Za zákonnost zpracování těchto údajů, včetně plnění informačních povinností vůči subjektům
+					údajů, a dále za to, že má oprávnění tyto údaje prostřednictvím Aplikace zadávat a
+					zpřístupnit je Provozovateli jakožto zpracovateli, odpovídá výhradně Uživatel jakožto
+					správce těchto osobních údajů.
+				</li>
+			</ol>
+			<li class="h4">Cookies</li>
+			<ol>
+				<li>
+					Pro zajištění správného a bezpečného fungování Aplikace jsou při jejím používání do
+					zařízení Uživatele ukládány nezbytné soubory cookies, a to zejména za účelem umožnění
+					ověření přihlášení, udržení aktivní relace, zajištění bezpečné komunikace mezi Uživatelem
+					a serverem Provozovatele nebo zapamatování základních uživatelských preferencí.
+				</li>
+				<li>
+					Tyto cookies jsou nastaveny automaticky a jejich používání je nezbytné pro poskytování
+					Služby. Vzhledem k jejich povaze není vyžadován souhlas Uživatele.
+				</li>
+				<li>
+					V případě, že by Provozovatel v budoucnu implementoval i jiné typy cookies (např. pro
+					analytické nebo marketingové účely), bude tak činit výhradně na základě předchozího
+					souhlasu Uživatele uděleného prostřednictvím příslušného nástroje pro správu souhlasů
+					(tzv. cookie lišty). O takové změně bude Uživatel předem vhodným způsobem informován.
+				</li>
+			</ol>
+			<li class="h4">Smlouva o zpracování osobních údajů</li>
+			<ol>
+				<li>
+					Tento oddíl tvoří smlouvu o zpracování osobních údajů ve smyslu čl. 28 Nařízení,
+					uzavíranou mezi Uživatelem jakožto správcem osobních údajů a Provozovatelem jakožto jejich
+					zpracovatelem.
+				</li>
+				<li>
+					Používáním Aplikace a odsouhlasením těchto zásad ochrany osobních údajů při registraci
+					Uživatelského účtu uzavírá Uživatel s Provozovatelem smlouvu o zpracování osobních údajů
+					dle tohoto oddílu. Provozovatel se zavazuje zpracovávat osobní údaje, které Uživatel
+					zadává do Aplikace, výhradně za účelem poskytování funkčnosti Služby, a to na základě
+					pokynů Uživatele jako správce.
+				</li>
+				<li>Provozovatel se dále zavazuje:</li>
+				<ol>
+					<li>zpracovávat osobní údaje pouze na základě dokumentovaných pokynů Uživatele,</li>
+					<li>přijmout vhodná technická a organizační opatření k zabezpečení osobních údajů,</li>
+					<li>
+						zajistit, že všechny osoby, které mají k osobním údajům přístup, jsou vázány povinností
+						mlčenlivosti,
+					</li>
+					<li>spolupracovat s Uživatelem při plnění povinností vůči subjektům údajů,</li>
+					<li>
+						po ukončení smluvního vztahu osobní údaje na pokyn Uživatele vymazat nebo mu je vrátit,
+						nebude-li v konkrétním případě právní povinnost postupovat jinak.
+					</li>
+				</ol>
+				<li>
+					Uživatel výslovně souhlasí s tím, že Provozovatel je oprávněn do zpracování zapojit další
+					zpracovatele, zejména poskytovatele hostingových a IT služeb uvedené v těchto zásadách.
+					Provozovatel odpovídá za to, že tito další zpracovatelé budou vázáni stejnými povinnostmi
+					jako Provozovatel podle této smlouvy.
+				</li>
+				<li>
+					Tato smlouva je uzavřena na dobu trvání smluvního vztahu mezi Uživatelem a Provozovatelem
+					a zaniká ukončením Uživatelského účtu.
+				</li>
+			</ol>
+			<li class="h4">Účinnost a změny zásad</li>
+			<ol>
+				<li>
+					Tyto zásady ochrany osobních údajů nabývají účinnosti dnem 20. 5. 2025. Provozovatel si
+					vyhrazuje právo tyto zásady kdykoli jednostranně změnit. O každé podstatné změně bude
+					Uživatel vhodným způsobem informován prostřednictvím Aplikace nebo e-mailové zprávy.
+				</li>
+			</ol>
+		</ol>
+	</div>
+</main>
+
+<style>
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: var(--padding);
+	}
+	.content {
+		max-width: 560px;
+		width: 100%;
+	}
+	li {
+		padding-bottom: var(--padding);
+	}
+	.h4 {
+		font-size: 1.5rem;
+		font-weight: 700;
+	}
+</style>
