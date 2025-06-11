a5cafbe5f5f0cdb4f85818cdb9122a3f937ccab2
Attila Gazso <230163+agazso@users.noreply.github.com>
2025-06-11 17:08:03 +0200

    fix: investment not found (#687)
    Fixes #682

--- src/routes/(app)/client/[id]/portfolio/[portfolio_id]/edit-investment/[investment_id]/+page@.svelte
+++ src/routes/(app)/client/[id]/portfolio/[portfolio_id]/edit-investment/[investment_id]/+page@.svelte
@@ -2,6 +2,7 @@
 	import { page } from '$app/state'
 	import EditInvestment from '$lib/components/edit-investment.svelte'
 	import Fullscreen from '$lib/components/fullscreen.svelte'
+	import Loader from '$lib/components/ui/loader.svelte'
 	import Typography from '$lib/components/ui/typography.svelte'
 	import { investmentStore } from '$lib/stores/investment.svelte'
 	import { portfolioStore } from '$lib/stores/portfolio.svelte'
@@ -20,7 +21,9 @@
 </script>
 
 <Fullscreen>
-	{#if portfolio && investment}
+	{#if portfolioStore.loading || investmentStore.loading}
+		<Loader />
+	{:else if portfolio && investment}
 		<EditInvestment {close} {portfolio} {investment} />
 	{:else}
 		<Typography variant="h2">404 - {$_('common.notFound')}</Typography>
