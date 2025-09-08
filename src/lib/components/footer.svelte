cb19abe4a68c711de87c08bdb241c6968542b7ef
Attila Gazso <230163+agazso@users.noreply.github.com>
2025-09-08 13:41:59 +0200

    fix: updated footer (#885)
    * fix: updated footer
    
    Fixes #872
    
    * fix: add env, exlude pattern
    
    * fix: typo
    
    * fix: add missing icon

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { PUBLIC_DISCORD_LINK } from '$env/static/public'
+	import { PUBLIC_DISCORD_LINK, PUBLIC_INSTAGRAM_LINK } from '$env/static/public'
 	import ResponsiveLayout from '$lib/components/ui/responsive-layout.svelte'
 	import Vertical from '$lib/components/ui/vertical.svelte'
 	import routes from '$lib/routes'
@@ -8,15 +8,31 @@
 	import FlexItem from './ui/flex-item.svelte'
 	import Typography from './ui/typography.svelte'
 	import { layoutStore } from '$lib/stores/layout.svelte'
+	import Button from './ui/button.svelte'
+	import Discord from './icons/discord.svelte'
+	import Instagram from './icons/instagram.svelte'
 </script>
 
 <Vertical class={`footer ${layoutStore.mobile ? `mobile` : ''}`} style="display: flex">
 	<ResponsiveLayout --responsive-gap="var(--double-padding)" --responsive-justify-content="center">
 		<Logo size={48} color="var(--colors-light-base)"></Logo>
-		<a href={PUBLIC_DISCORD_LINK}>Discord</a>
-		<a href={routes.TERMS}>{$_('component.footer.termsOfService')}</a>
-		<a href={routes.PRIVACY}>{$_('component.footer.privacyPolicy')}</a>
-		<FlexItem />
+		{#if !layoutStore.mobile}
+			<FlexItem />
+		{/if}
+		<ResponsiveLayout --responsive-gap="var(--half-padding)" --responsive-justify-content="center">
+			<Button variant="ghost" mode="dark" href={PUBLIC_DISCORD_LINK} target="_blank"
+				><Discord size={24} />Discord</Button
+			>
+			<Button variant="ghost" mode="dark" href={PUBLIC_INSTAGRAM_LINK} target="_blank"
+				><Instagram size={24} />Instagram</Button
+			>
+			<Button variant="ghost" mode="dark" href={routes.TERMS} target="_blank"
+				>{$_('component.footer.termsOfService')}</Button
+			>
+			<Button variant="ghost" mode="dark" href={routes.PRIVACY} target="_blank"
+				>{$_('component.footer.privacyPolicy')}</Button
+			>
+		</ResponsiveLayout>
 		<Typography --typography-color="var(--colors-light-base)" style="opacity: 0.5"
 			>© 2025 Kalkul.app s.r.o.</Typography
 		>
@@ -32,9 +48,5 @@
 	}
 	:global(.footer.mobile) {
 		padding: var(--quadruple-padding) var(--double-padding);
-	}
-	a {
-		color: var(--colors-light-base);
-		text-decoration: underline;
 	}
 </style>
